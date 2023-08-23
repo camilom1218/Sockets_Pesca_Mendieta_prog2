ec25c522ccab596394b1bbe80efb25f2a12d519e
Camilo Mendieta <Camilo Mendieta@Acer-56Y6-AK170>
2023-08-22 20:30:34 -0500

    Version 1.1 In this version create the package and the "server" class, we also declare the variables and create constructor and run method.

--- ServidorMendieta/src/Servidor/Servidor.java
+++ ServidorMendieta/src/Servidor/Servidor.java
@@ -1,9 +1,24 @@
 package Servidor;
 
-public class Servidor {
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor extends Thread {
+
+	private Socket socket;
+	private ServerSocket server;
+	private int port;
+
+	public Servidor(int port) {
+		this.server = server;
+		this.port = port;
+	}
+
+	public void run() {
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
